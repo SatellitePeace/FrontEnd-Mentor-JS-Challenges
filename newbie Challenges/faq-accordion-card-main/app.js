8c5cc2b784ac26897b714331113de1dd2e69a50f
Njoku <njokunneoma1999@gmail.com>
2022-09-10 13:02:00 +0200

    js file for FAQ functionalities

--- newbie Challenges/faq-accordion-card-main/app.js
+++ newbie Challenges/faq-accordion-card-main/app.js
@@ -1,6 +1,7 @@
 const answers = document.querySelectorAll(".answers");
 const question = document.querySelectorAll(".question");
 const faq = document.querySelectorAll(".FAQ");
+const arrow = document.querySelectorAll(".arrow");
 
 question.forEach((item, index) => {
   item.addEventListener("click", function () {
@@ -10,10 +11,12 @@ question.forEach((item, index) => {
       item.classList.remove("show");
       answers[index].classList.remove("show");
       faq[index].classList.remove("show");
+      arrow[index].classList.remove("show");
     } else {
       item.classList.add("show");
       answers[index].classList.add("show");
       faq[index].classList.add("show");
+      arrow[index].classList.add("show");
     }
   });
 });
@@ -21,4 +24,5 @@ question.forEach((item, index) => {
 function hideAllContents() {
   answers.forEach((content) => content.classList.remove("show"));
   faq.forEach((content) => content.classList.remove("show"));
+  arrow.forEach((content) => content.classList.remove("show"));
 }
