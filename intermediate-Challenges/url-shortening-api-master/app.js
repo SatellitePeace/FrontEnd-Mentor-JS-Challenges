ed3ea7ac84d4ca037d1886567c9289c81dc231eb
Njoku <njokunneoma1999@gmail.com>
2022-10-04 21:37:31 +0200

    complete challenge solutions

--- intermediate-Challenges/url-shortening-api-master/app.js
+++ intermediate-Challenges/url-shortening-api-master/app.js
@@ -6,7 +6,6 @@ openAndCloseSmallnav();
 function openAndCloseSmallnav() {
   smallnavBtn.addEventListener("click", () => {
     smallnavContents.classList.toggle("show");
-    console.log(123);
   });
 }
 
@@ -19,45 +18,39 @@ const form = document.getElementById("form");
 const urlContainer = document.querySelector(".url__list");
 const urlSection = document.querySelector(".url");
 
-validation();
-function validation() {
+validateForm();
+function validateForm() {
   form.addEventListener("submit", (e) => {
-    let link = input.value;
-    if (link == "") {
+    const inputValue = input.value;
+    if (inputValue === "") {
       input.classList.add("error");
       errorMsg.classList.add("alert");
     } else {
       input.classList.remove("error");
       errorMsg.classList.remove("alert");
       input.value = "";
-      short(link);
-      copyClipboard(link);
-      addToLocalStorage(link);
-      getLocalStorage(link);
+      shortenUrl(inputValue);
+      addToLocalStorage(inputValue);
     }
     e.preventDefault();
   });
 }
-// FETCH SHORTCODE API
-async function short(input) {
+
+async function shortenUrl(input) {
   const url = `https://api.shrtco.de/v2/shorten?url=${input}`;
   const response = await fetch(url);
   const data = await response.json();
-
-  // grant ui function access to api
-  showUrl(data);
-  getLocalStorage(data);
+  displayURl(data);
 }
-// URL UI
-function showUrl(data) {
+
+function displayURl(data) {
   const original_link = data.result.original_link;
   const short_url = data.result.full_short_link;
   // dynamically add html to DOM and UI
   const article = document.createElement("article");
-
   article.className = "url__shorten";
   article.innerHTML = `<div class="url__original">
-              <p class="url__text">
+       <p class="url__text">
               ${original_link}
               </p>
             </div>
@@ -72,30 +65,15 @@ function showUrl(data) {
                 <i class="bi bi-trash3"></i></a>
             </div>`;
   urlSection.appendChild(article);
-
   // copy shortend link funtion
-  copyClipboard(article);
+  copyToClipboard(article);
   // delete url
   deleteUrl(article);
-  // LOCAL STORAGE
-  addToLocalStorage(article);
-  getLocalStorage(article);
-}
-// DELETE URL FROM DISPLAY AND LS
-function deleteUrl(e) {
-  const articleContainer = document.querySelectorAll(".url__shorten");
-  const deleteBtn = document.querySelectorAll(".url__delete");
-  for (let i = 0; i < deleteBtn.length; i++) {
-    deleteBtn[i].addEventListener("click", (e) => {
-      articleContainer[i].remove();
-      removeLocalStorage();
-      e.preventDefault();
-    });
-  }
 }
 
-// COPY SHORTENED URL TO CLIPBOARD
-function copyClipboard(e) {
+// COPY SHORT URL TO CLIPBOARD
+
+function copyToClipboard() {
   const copyLink = document.querySelectorAll(".url__clipboard");
   const shortLink = document.querySelectorAll(".url__after--text");
   for (let i = 0; i < copyLink.length; i++) {
@@ -103,15 +81,31 @@ function copyClipboard(e) {
       navigator.clipboard.writeText(shortLink[i].textContent);
       copyLink[i].style.background = "hsl(257, 27%, 26%)";
       copyLink[i].textContent = "Copied!";
+      // remove new button styles after 2s
+      setTimeout(() => {
+        copyLink[i].style.background = "hsl(180, 66%, 49%)";
+        copyLink[i].textContent = "Copy";
+      }, 2000);
       e.preventDefault();
     });
   }
 }
 
-// LOCAL STORAGE
+// DELETE URL FROM DISPLAY AND LS
+function deleteUrl() {
+  const articleContainer = document.querySelectorAll(".url__shorten");
+  const deleteBtn = document.querySelectorAll(".url__delete");
+  for (let i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener("click", (e) => {
+      articleContainer[i].remove();
+      e.preventDefault();
+    });
+  }
+}
+
+// ADD TO LOCAL STORAGE
 function addToLocalStorage(todo) {
   let todos;
-
   if (localStorage.getItem("todos") === null) {
     todos = [];
   } else {
@@ -119,54 +113,4 @@ function addToLocalStorage(todo) {
   }
   todos.push(todo);
   localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-window.addEventListener("DOMContentLoaded", getLocalStorage);
-
-function getLocalStorage(data, article) {
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
-  }
-
-  // const original_link = data.result.original_link;
-  // const short_url = data.result.full_short_link;
-  todos.forEach((todo) => {
-    // const original_link = data.result.original_link;
-    // const short_url = data.result.full_short_link;
-    // dynamically add html to DOM and UI
-    const article = document.createElement("article");
-
-    article.className = "url__shorten";
-    article.innerHTML = `<div class="url__original">
-              <p class="url__text">
-              ${todo}
-              </p>
-            </div>
-           <div class="url__after">
-              <p class="url__after--text">
-              ${todo}
-              </p>
-              <button class="url__clipboard">
-                Copy
-              </button>
-              <a href="#" class="url__delete">
-                <i class="bi bi-trash3"></i></a>
-            </div>`;
-    urlSection.appendChild(article);
-  });
-}
-
-function removeLocalStorage(todo) {
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
-  }
-  // const todoIndex = todo.children[0].innerText;
-  // todos.splice(todos.indexOf(todoIndex), 1);
-  // localStorage.setItem("todos", JSON.stringify(todos));
 }
